5f84c91d7270087a3de3105ce40a47d52cb05305
bmisra21 <42977074+bmisra21@users.noreply.github.com>
2019-06-06 12:58:22 -0400

    Update Level3.java

--- src/Level3.java
+++ src/Level3.java
@@ -2,6 +2,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -25,27 +26,43 @@ public class Level3 extends JPanel implements ActionListener
 	private Platform platform12;
 	private Platform platform13;
 	private Platform platform14;
-	private int num =1;
-	Hero hero;
+	private int num1 =1;
+	private int num2 =1;
+	private Hero hero;
 	private Rectangle safeFloor;
 	private Walls wall1;
 	private Lava lava;
+	private ArrayList <Enemy> enemies = new ArrayList<Enemy>();
+	private Enemy enemy1;
 	
 	public Level3()
 	{
 		setLayout(null);
-		Timer timer = new Timer(50, this);
+		Timer timer = new Timer(16, this);
 		timer.start();
 		hero = new Hero();
-		hero.setBounds(30, 20, 50, 90);
+		hero.setBounds(350, 420, 50, 90);
 		add(hero);
+		/**
+		for (int i=0; i<2; i++)
+		{
+			enemies.add(new Enemy());
+		}
+		
+		enemies.get(0).setBounds(350, 600, 24, 30);
+		add(enemies.get(0));
+		enemies.get(1).setBounds(600, 600, 24, 30);
+		add(enemies.get(1));**/
+		enemy1 = new Enemy();
+		enemy1.setBounds(350, 600, 24, 30);
+		add(enemy1);
 		
 		platform1 = new Platform(0, 110, 60, 20);
 		platform2 = new Platform(140, 300, 60, 20);
 		platform3 = new Platform(200, 550, 60, 20);
-		platform4 = new Platform(350, 500, 60, 20);
-		platform5 = new Platform(600, 300, 60, 20);
-		platform6 = new Platform(500, 500, 60, 20);
+		platform4 = new Platform(350, 510, 60, 20);
+		platform5 = new Platform(300, 300, 60, 20);
+		platform6 = new Platform(500, 510, 60, 20);
 		platform7 = new Platform(650, 550, 60, 20);
 		platform8 = new Platform(950, 550, 60, 20);
 		platform9 = new Platform(1210, 500, 60, 20);
@@ -53,6 +70,10 @@ public class Level3 extends JPanel implements ActionListener
 		platform11 = new Platform(950, 300, 60, 20);
 		platform12 = new Platform(800, 200, 60, 20);
 		platform13 = new Platform(650, 100, 60, 20);
+		
+		Bullets bullet = new Bullets();
+		bullet.setBounds(800, 300, 20, 20);
+		add(bullet);
 		
 		wall1 = new Walls(200, 0, 20, 400);
 		lava = new Lava(100, 400, 1000, 20);
@@ -89,23 +110,33 @@ public class Level3 extends JPanel implements ActionListener
 	
 	public void actionPerformed(ActionEvent e) 
 	{
+		
 		if (platform5.getX() > 450)
-			num=-1;
+			num1=-1;
 		if(platform5.getX() <300)
-			num=1;
-		if (num==1)
+			num1=1;
+		if (num1==1)
 			platform5.setDx(5);
 		else platform5.setDx(-5);
 		
+		if (enemy1.getX() > 450)
+			num2=-1;
+		if(enemy1.getX() <350)
+			num2=1;
+		if (num2==1)
+			enemy1.setDx(5);
+		else enemy1.setDx(-5);
 		
-		
+		enemy1.update();
 		
 		platform5.update();
-		
 		repaint(platform5);
+		revalidate();
 		repaint();
+		
 	}
 
 	
 
 }
+
