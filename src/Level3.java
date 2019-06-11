be3c44623f9f64c2ad232ab62018173f16cefbff
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2019-06-10 23:01:23 -0400

    Update Level3.java

--- src/Level3.java
+++ src/Level3.java
@@ -34,11 +34,13 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	private Platform platform11;
 	private Platform platform12;
 	private Platform platform13;
+	private Platform platform14;
 	private int num1 =1;
 	private Hero hero;
 	private Platform safeFloor;
 	private Walls wall1;
 	private Lava lava;
+	private Lava lava2;
 	private ArrayList<Bullets> bullets = new ArrayList<Bullets>();
 	private Enemy enemy1;
 	private Enemy enemy2;
@@ -48,6 +50,12 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	private Timer timer;
 	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 	private ArrayList<Platform> platforms = new ArrayList<Platform>();
+	private boolean isJumping = false;
+	private int jumpTime = 0;
+	private boolean onPlatform = true;
+	private int fallSpeed = 1;
+	private int delay = 0;
+
 	
 	public Level3()
 	{
@@ -100,29 +108,33 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		g2.fill(platform11);
 		g2.fill(platform12);
 		g2.fill(platform13);
+		g2.fill(platform14);
 		g2.fill(wall1);
 		g2.fill(safeFloor);
 		g2.setColor(Color.red);
 		g2.fill(lava);
+		g2.fill(lava2);
 		g2.setColor(Color.blue);
 		g2.fill(platform5);
+		
 		
 	}
 	public void addPlatforms()
 	{
 		platform1 = new Platform(0, 110, 60, 20);
 		platform2 = new Platform(140, 300, 60, 20);
-		platform3 = new Platform(200, 600, 60, 20);
-		platform4 = new Platform(350, 510, 60, 20);
+		platform3 = new Platform(800, 600, 60, 20);
+		platform4 = new Platform(350, 600, 60, 20);
 		platform5 = new Platform(300, 300, 60, 20);
-		platform6 = new Platform(500, 510, 60, 20);
+		platform6 = new Platform(500, 540, 60, 20);
 		platform7 = new Platform(650, 580, 60, 20);
 		platform8 = new Platform(950, 580, 60, 20);
 		platform9 = new Platform(1210, 500, 60, 20);
-		platform10 = new Platform(1100, 400, 60, 20);
-		platform11 = new Platform(950, 300, 60, 20);
-		platform12 = new Platform(800, 200, 60, 20);
-		platform13 = new Platform(650, 100, 60, 20);
+		platform10 = new Platform(1050, 400, 110, 20);
+		platform11 = new Platform(950, 325, 60, 20);
+		platform12 = new Platform(800, 250, 60, 20);
+		platform13 = new Platform(650, 200, 60, 20);
+		platform14 = new Platform(1100, 540, 60, 20);
 		platforms.add(platform1);
 		platforms.add(platform2);
 		platforms.add(platform3);
@@ -136,33 +148,35 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		platforms.add(platform11);
 		platforms.add(platform12);
 		platforms.add(platform13);
+		platforms.add(platform14);
 		
 		wall1 = new Walls(200, 0, 20, 400);
-		lava = new Lava(150, 400, 950, 20);
-		safeFloor = new Platform(0,640, 1280, 20);
+		lava = new Lava(200, 400, 850, 20);
+		lava2 = new Lava(300, 637, 970, 20);
+		safeFloor = new Platform(0,637, 300, 20);
 		
 	}
 	public void addEnemies()
 	{
 		enemy1 = new Enemy(350, 450);
-		enemy1.setBounds(350, 600, 24, 30);
+		enemy1.setBounds(350, 550, 24, 30);
 		add(enemy1);
 		enemy2 = new Enemy(800, 900);
-		enemy2.setBounds(800, 250, 24, 30);
+		enemy2.setBounds(800, 200, 24, 30);
 		add(enemy2);
-		enemy3 = new Enemy(650, 750);
+		/*enemy3 = new Enemy(650, 750);
 		enemy3.setBounds(650, 150, 24, 30);
-		add(enemy3);
-		enemy4 = new Enemy(950, 1050);
-		enemy4.setBounds(950, 350, 24, 30);
-		add(enemy4);
+		add(enemy3);*/
+		/*enemy4 = new Enemy(950, 1050);
+		enemy4.setBounds(950, 300, 24, 30);
+		add(enemy4);*/
 		enemy5 = new Enemy(750, 950);
 		enemy5.setBounds(750, 500, 24, 30);
 		add(enemy5);
 		enemies.add(enemy1);
 		enemies.add(enemy2);
-		enemies.add(enemy3);
-		enemies.add(enemy4);
+		//enemies.add(enemy3);
+		//enemies.add(enemy4);
 		enemies.add(enemy5);
 		
 	}
@@ -199,11 +213,11 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	{
 		if (hero.getX() <= 0)
 		{
-				hero.setDx(3);
+				hero.setLocation(0, hero.getY());
 		}
 		else if(hero.getX() >= 1200)
 		{
-			hero.setDx(-3);
+			hero.setLocation(1200, hero.getY());
 		}
 		if (hero.getY() <= 0)
 		{
@@ -226,27 +240,7 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 				hero.setDy(3);
 			}
 		}
-		for(Platform platform: platforms)
-		{
-			if(platform.isTouched(hero))
-			{
-				if(platform == platform1)
-					break;
-				if(platform==platform2)
-				{
-					hero.setDx(-3);
-					hero.setDy(-3);
-				}
-				if(platform.getY() >= hero.getY())
-					hero.setDy(-3);
-				if(platform.getY() <= hero.getY())
-					hero.setDy(3);
-				if(platform.getX() >= hero.getX())
-					hero.setDx(-3);
-				if(platform.getX() <= hero.getX())
-					hero.setDx(3);
-			}	
-		}
+		
 	}
 	public void checkEnemies()
 	{
@@ -294,10 +288,10 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		enemy1.update();
 		moveEnemy(enemy2);
 		enemy2.update();
-		moveEnemy(enemy3);
-		enemy3.update();
-		moveEnemy(enemy4);
-		enemy4.update();
+		//moveEnemy(enemy3);
+		//enemy3.update();
+		//moveEnemy(enemy4);
+		//enemy4.update();
 		moveEnemy(enemy5);
 		enemy5.update();
 		
@@ -315,6 +309,60 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 			gameOver.setBounds(250, 50, 400, 200);
 			add(gameOver);
 			timer.stop();
+		}
+		if(lava2.isTouched(hero))
+		{
+			JLabel gameOver = new JLabel("Game Over");
+			gameOver.setFont(gameOver.getFont().deriveFont(40.0f));
+			gameOver.setBounds(250, 50, 400, 200);
+			add(gameOver);
+			timer.stop();
+		}
+		if((hero.getDy() == -5) || hero.getDy() == -3)
+		{
+			jumpTime ++;
+			isJumping = true;
+			if (jumpTime == 15)
+			{
+				jumpTime = 0;
+				isJumping = false;
+				
+				fallSpeed = 1;
+			}
+			
+		}
+		for(Platform platform: platforms)
+		{
+			if(platform.isTouched(hero))
+			{
+				onPlatform = true;
+				fallSpeed = 1;
+				if(platform.getY() <= hero.getY())
+					hero.setY(platform.getY() + 25);
+				else if(platform.getY() >= hero.getY() + 60)
+					hero.setY(platform.getY() - 80);
+				else if(platform.getX() >= hero.getX())
+				{
+					hero.setDx(0);
+				}
+				else if(platform.getX() <= hero.getX())
+				{
+					hero.setDx(0);
+				}
+			}
+			else
+			{
+				onPlatform = false;
+			}				
+		}
+		if ((hero.getY() < 550) && !(isJumping) && !onPlatform)
+		{
+			hero.setDy(fallSpeed);
+			delay ++;
+			if (delay %2 == 0)
+			{
+				fallSpeed++;
+			}
 		}
 		checkBounds();
 		hero.update();
