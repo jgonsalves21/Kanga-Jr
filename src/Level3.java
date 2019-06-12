6b5ac079a5956251ff54686ae489afd14c104ff6
bmisra21 <42977074+bmisra21@users.noreply.github.com>
2019-06-12 19:38:50 -0400

    Update Level3.java

--- src/Level3.java
+++ src/Level3.java
@@ -3,6 +3,8 @@ import java.awt.Color;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.MouseInfo;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Shape;
 import java.awt.event.ActionEvent;
@@ -19,7 +21,9 @@ import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.Timer;
 import java.awt.event.KeyListener;
-public class Level3 extends Levels//extends JPanel implements ActionListener, Level
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+public class Level3 extends Levels implements ActionListener,  MouseListener
 {
 	private Platform platform1;
 	private Platform platform2;
@@ -41,7 +45,6 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	private Walls wall1;
 	private Lava lava;
 	private Lava lava2;
-	private ArrayList<Bullets> bullets = new ArrayList<Bullets>();
 	private Enemy enemy1;
 	private Enemy enemy2;
 	private Enemy enemy3;
@@ -50,11 +53,10 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	private Timer timer;
 	private ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 	private ArrayList<Platform> platforms = new ArrayList<Platform>();
-	private boolean isJumping = false;
-	private int jumpTime = 0;
-	private boolean onPlatform = true;
-	private int fallSpeed = 1;
-	private int delay = 0;
+	private ArrayList<Bullets> bullets = new ArrayList<Bullets>();
+	private ArrayList<Walls> walls = new ArrayList<Walls>();
+	private boolean onGround = false;
+	private double yVel = 0;
 
 	
 	public Level3()
@@ -63,8 +65,7 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		this.setFocusable(true);
 		this.requestFocusInWindow();
 		keySensing();
-	
-		
+		addMouseListener(this);
 		timer = new Timer(16, this);
 		timer.start();
 		hero = new Hero();
@@ -76,20 +77,53 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	}
 	public void keySensing()
 	{
-		int mapName = WHEN_IN_FOCUSED_WINDOW;
-		InputMap imap= getInputMap(mapName);
-		KeyStroke right = KeyStroke.getKeyStroke('d');
-		imap.put(right, "moveR");
-		KeyStroke left = KeyStroke.getKeyStroke('a');
-		imap.put(left, "moveL");
-		KeyStroke space = KeyStroke.getKeyStroke(' ');
-		imap.put(space, "shoot");
-		KeyStroke up = KeyStroke.getKeyStroke('w');
-		imap.put(up, "moveU");
-		KeyStroke down = KeyStroke.getKeyStroke('s');
-		imap.put(down, "moveD");
-		KeyStroke reset = KeyStroke.getKeyStroke('r');
-		imap.put(reset, "reset");
+		InputMap im = getInputMap(WHEN_IN_FOCUSED_WINDOW);
+        ActionMap am = getActionMap();
+
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, false), "rightpressed");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_D, 0, true), "rightreleased");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, false), "leftpressed");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_A, 0, true), "leftreleased");
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_W, 0, false), "uppressed");
+        
+
+        am.put("rightpressed", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) 
+            {
+                hero.setDx(8);
+            }
+        });
+
+        am.put("rightreleased", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) 
+            {
+                hero.setDx(0);
+            }
+        });
+        am.put("leftpressed", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) 
+            {
+                hero.setDx(-8);
+            }
+        });
+
+        am.put("leftreleased", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) 
+            {
+                hero.setDx(0);
+            }
+        });
+        am.put("uppressed", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) 
+            {
+            	System.out.println(onGround);
+            	if(onGround) {
+	            	onGround = false;
+	                yVel = -15;
+            	}
+                
+            }
+        });
 	}
 	
 	public void paintComponent(Graphics g)
@@ -135,6 +169,7 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		platform12 = new Platform(800, 250, 60, 20);
 		platform13 = new Platform(650, 200, 60, 20);
 		platform14 = new Platform(1100, 540, 60, 20);
+		safeFloor = new Platform(0,637, 300, 20);
 		platforms.add(platform1);
 		platforms.add(platform2);
 		platforms.add(platform3);
@@ -149,34 +184,36 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		platforms.add(platform12);
 		platforms.add(platform13);
 		platforms.add(platform14);
+		platforms.add(safeFloor);
 		
 		wall1 = new Walls(200, 0, 20, 400);
+		walls.add(wall1);
 		lava = new Lava(200, 400, 850, 20);
 		lava2 = new Lava(300, 637, 970, 20);
-		safeFloor = new Platform(0,637, 300, 20);
+		
 		
 	}
 	public void addEnemies()
 	{
 		enemy1 = new Enemy(350, 450);
-		enemy1.setBounds(350, 550, 24, 30);
+		enemy1.setBounds(350, 500, 24, 30);
 		add(enemy1);
 		enemy2 = new Enemy(800, 900);
-		enemy2.setBounds(800, 200, 24, 30);
+		enemy2.setBounds(800, 210, 24, 30);
 		add(enemy2);
-		/*enemy3 = new Enemy(650, 750);
-		enemy3.setBounds(650, 150, 24, 30);
-		add(enemy3);*/
-		/*enemy4 = new Enemy(950, 1050);
-		enemy4.setBounds(950, 300, 24, 30);
-		add(enemy4);*/
+		enemy3 = new Enemy(650, 750);
+		enemy3.setBounds(650, 160, 24, 30);
+		add(enemy3);
+		enemy4 = new Enemy(950, 1050);
+		enemy4.setBounds(950, 290, 24, 30);
+		add(enemy4);
 		enemy5 = new Enemy(750, 950);
 		enemy5.setBounds(750, 500, 24, 30);
 		add(enemy5);
 		enemies.add(enemy1);
 		enemies.add(enemy2);
-		//enemies.add(enemy3);
-		//enemies.add(enemy4);
+		enemies.add(enemy3);
+		enemies.add(enemy4);
 		enemies.add(enemy5);
 		
 	}
@@ -213,7 +250,7 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	{
 		if (hero.getX() <= 0)
 		{
-				hero.setLocation(0, hero.getY());
+			hero.setLocation(0, hero.getY());
 		}
 		else if(hero.getX() >= 1200)
 		{
@@ -221,24 +258,49 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		}
 		if (hero.getY() <= 0)
 		{
-				hero.setDy(3);
+				hero.setLocation(hero.getX(),0);
 		}
-		else if(hero.getY() >= 550)
+		else if(hero.getY() >= 560)
 		{
-			hero.setLocation(hero.getX(), 550);
+			hero.setLocation(hero.getX(), 560);
 		}
-		else if(wall1.isTouched(hero))
+		
+		for(Walls wall: walls)
 		{
-			if(hero.getX()<=200)
+			if(wall.isTouched(hero))
 			{
-				hero.setDx(-3);
-				hero.setDy(3);
+				if (wall.getLength() == 25)
+				{
+					if(hero.getX()<=wall.getX())
+					{
+						hero.setLocation((int)(wall.getX() - 51), hero.getY());
+					}
+					else if(hero.getY() <= wall.getY())
+					{
+						onGround = true;
+					}
+					else if(hero.getX() >= wall.getX())
+					{
+						hero.setLocation((int)(wall.getX() + 26), hero.getY());
+					}
+				}
+				else 
+				{
+					if(hero.getX()<=wall.getX())
+					{
+						hero.setLocation((int)(wall.getX() - 50), hero.getY());
+					}
+					else if(hero.getY() <= wall.getY())
+					{
+						onGround = true;
+					}
+					else if(hero.getX() >= wall.getX())
+					{
+						hero.setLocation((int)(wall.getX() + 200), hero.getY());
+					}
+				}
 			}
-			else
-			{
-				hero.setDx(3);
-				hero.setDy(3);
-			}
+			
 		}
 		
 	}
@@ -246,12 +308,12 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	{
 		for (Enemy enemy: enemies)
 		{
-			for(Bullets bullet : bullets)
+			for(int i = bullets.size()-1; i>=0; i--)
 			{
-				if (enemy.isShot(bullet))
+				if (enemy.isShot(bullets.get(i)))
 				{
 					remove(enemy);
-					remove(bullet);
+					remove(bullets.get(i));
 					enemy.Kill();
 					break;
 				}
@@ -271,14 +333,46 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 			if(!enemies.get(i).lifeStatus())
 				enemies.remove(i);
 		}
-		if(enemies.isEmpty() && platform5.isTouched(hero))
+	}
+	public int checkBullets()
+	{
+		if(bullets.isEmpty()==true)
 		{
-			JLabel gameOver = new JLabel("YOU WON!");
-			gameOver.setFont(gameOver.getFont().deriveFont(40.0f));
-			gameOver.setBounds(250, 50, 400, 200);
-			add(gameOver);
-			timer.stop();
+			return 0;
 		}
+
+		for (int i= bullets.size()-1; i>=0; i--)
+		{
+			for(Walls wall: walls)
+			{
+				if(wall.isShot(bullets.get(i)))
+				{
+					remove(bullets.get(i));
+					bullets.remove(bullets.get(i));
+					return 0;
+				}
+			}
+
+		}
+		if(bullets.isEmpty()==true)
+		{
+			return 0;
+		}
+		for (int i= bullets.size()-1; i>=0; i--)
+		{
+			for(Platform platform: platforms)
+			{
+				if(platform.isShot(bullets.get(i)))
+				{
+					remove(bullets.get(i));
+					bullets.remove(bullets.get(i));
+					return 0;
+				}
+			}
+
+		}
+		return 0;		
+		
 	}
 	public void actionPerformed(ActionEvent e) 
 	{
@@ -288,10 +382,10 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 		enemy1.update();
 		moveEnemy(enemy2);
 		enemy2.update();
-		//moveEnemy(enemy3);
-		//enemy3.update();
-		//moveEnemy(enemy4);
-		//enemy4.update();
+		moveEnemy(enemy3);
+		enemy3.update();
+		moveEnemy(enemy4);
+		enemy4.update();
 		moveEnemy(enemy5);
 		enemy5.update();
 		
@@ -301,7 +395,7 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 			
 		}
 		checkEnemies();
-		
+		checkBullets();
 		if(lava.isTouched(hero))
 		{
 			JLabel gameOver = new JLabel("Game Over");
@@ -318,58 +412,88 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 			add(gameOver);
 			timer.stop();
 		}
-		if((hero.getDy() == -5) || hero.getDy() == -3)
-		{
-			jumpTime ++;
-			isJumping = true;
-			if (jumpTime == 15)
-			{
-				jumpTime = 0;
-				isJumping = false;
-				
-				fallSpeed = 1;
-			}
-			
-		}
+		onGround=false;
 		for(Platform platform: platforms)
 		{
-			if(platform.isTouched(hero))
+			
+			if(platform.isTouched(hero) && yVel >= 0)
 			{
-				onPlatform = true;
-				fallSpeed = 1;
-				if(platform.getY() <= hero.getY())
-					hero.setY(platform.getY() + 25);
-				else if(platform.getY() >= hero.getY() + 60)
+				
+				if(platform.getY() >= hero.getY() + 70)
+				{
 					hero.setY(platform.getY() - 80);
-				else if(platform.getX() >= hero.getX())
+					onGround = true;
+				}
+				else if(platform.getX() >= hero.getX()-5)
 				{
 					hero.setDx(0);
+					hero.setLocation((int)(platform.getX()-55), hero.getY());
 				}
+					
 				else if(platform.getX() <= hero.getX())
-				{
 					hero.setDx(0);
+			}
+			else if ((platform.isTouched(hero) && yVel <= 0))
+			{
+				if((platform.getY() + 1 <= hero.getY()) && (platform.getY() + 20 >= hero.getY()))
+				{
+					yVel = 0;
 				}
+					
 			}
-			else
-			{
-				onPlatform = false;
-			}				
 		}
-		if ((hero.getY() < 550) && !(isJumping) && !onPlatform)
+		
+		if(onGround) 
 		{
-			hero.setDy(fallSpeed);
-			delay ++;
-			if (delay %2 == 0)
-			{
-				fallSpeed++;
-			}
+			yVel = 0;
 		}
+		if ((hero.getY() < 550) && !onGround)
+		{
+			yVel += 1;
+			
+		}
+		if(yVel > 10)
+			yVel=10;
+		hero.setDy(yVel);
+		System.out.println(onGround);
 		checkBounds();
 		hero.update();
-		platform5.update();
-		repaint(platform5);
 		revalidate();
 		repaint();
+		
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		Point mouseLocation = MouseInfo.getPointerInfo().getLocation();
+		Bullets nbullet = new Bullets(hero.getX()+25, hero.getY()+45, mouseLocation);
+		nbullet.setBounds(hero.getX()+25, hero.getY()+45, 10, 10);
+		bullets.add(nbullet);
+		add(nbullet);
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -380,3 +504,4 @@ public class Level3 extends Levels//extends JPanel implements ActionListener, Le
 	
 
 }
+
